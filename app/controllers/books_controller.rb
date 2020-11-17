91c1e0422f71ed6faf9eda10712335e42f0d8a7d
EC2 Default User <ec2-user@ip-172-31-21-33.ec2.internal>
2020-11-17 12:57:32 +0000

    [Finish]応用課題２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,14 @@
 class BooksController < ApplicationController
+  # before_action :ensure_correct_user, only: [:edit]
 
   def show
     @book = Book.find(params[:id])
+    @book_post = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -15,12 +18,16 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      # @book = Book.new
+      render :index
     end
   end
 
   def edit
     @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 
   def update
@@ -32,16 +39,22 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title,:body)
   end
-
+  
+  # def ensure_correct_user
+  #   @user = User.find(params[:id])
+  #   unless @user == current_user
+  #     redirect_to user_path(current_user)
+  #   end
+  # end
 end
